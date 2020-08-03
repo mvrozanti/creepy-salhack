04939448401b117e2ab1385cddc78b8e654b1193
CreepyOrb924 <41650640+CreepyOrb924@users.noreply.github.com>
2020-08-02 22:46:11 -0400

    Update SalHackMod.java

--- src/main/java/me/ionar/salhack/SalHackMod.java
+++ src/main/java/me/ionar/salhack/SalHackMod.java
@@ -12,20 +12,22 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "salhack", name = "SalHack", version = SalHackMod.VERSION)
+Mod(modid = "creepy-salhack", name = "Creepy SalHack", version = SalHackMod.VERSION)
 public final class SalHackMod
 {
-    public static final String NAME = "SalHack";
-    public static final String VERSION = "2.05";
+    public static final String NAME = "Creepy SalHack";
+    public static final String VERSION = "2.06.4";
 
     public static final Logger log = LogManager.getLogger("sal");
 
     public static final EventBus EVENT_BUS = new EventManager();
+    public static Blur instance;
+
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-        log.info("init salhack v: " + VERSION);
+        log.info("init creepy-salhack v: " + VERSION);
 
         SalHack.Init();
 
