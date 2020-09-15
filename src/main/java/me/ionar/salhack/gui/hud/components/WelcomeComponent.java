d3b6cdbdd462d258cc0d3dabc96c2340a631e0c0
CreepyOrb924 <41650640+CreepyOrb924@users.noreply.github.com>
2020-09-14 21:54:18 -0400

    Time welcome

--- src/main/java/me/ionar/salhack/gui/hud/components/WelcomeComponent.java
+++ src/main/java/me/ionar/salhack/gui/hud/components/WelcomeComponent.java
@@ -1,59 +1,52 @@
 package me.ionar.salhack.gui.hud.components;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
-
-import me.ionar.salhack.SalHackMod;
 import me.ionar.salhack.gui.hud.HudComponentItem;
-import me.ionar.salhack.main.Wrapper;
-import me.ionar.salhack.module.Value;
 import me.ionar.salhack.util.render.RenderUtil;
-import me.ionar.salhack.gui.hud.HudComponentItem;
-import me.ionar.salhack.managers.ImageManager;
-import me.ionar.salhack.managers.TickRateManager;
-import me.ionar.salhack.util.imgs.SalDynamicTexture;
-import me.ionar.salhack.util.render.RenderUtil;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.inventory.GuiInventory;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.OpenGlHelper;
-import net.minecraft.client.renderer.RenderHelper;
 
+import java.util.Calendar;
 
 public class WelcomeComponent extends HudComponentItem
 {
-    public final Value<Boolean> Reliant = new Value<Boolean>("Reliant", new String[]
-            { "" }, "Shows reliant text instead of salhack", false);
-
-    private String WelcomeString = "Welcome " + ChatFormatting.WHITE + mc.getSession().getUsername();
-
+    //I coded this but SalHack skid coded it better so props to https://github.com/pleasegivesource/SalHackSkid for this.
     public WelcomeComponent()
     {
-        super("Welcome", 5, 5);
-        SetHidden(false);
+        super("WelcomeComponent", 200, 2);
     }
 
-
+    String l_welcome = "";
+    Calendar c = Calendar.getInstance();
+    int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
 
     @Override
     public void render(int p_MouseX, int p_MouseY, float p_PartialTicks)
     {
         super.render(p_MouseX, p_MouseY, p_PartialTicks);
 
-        if (Reliant.getValue())
+        if (timeOfDay >= 6 && timeOfDay < 12)
         {
-            final String l_Text = "Reliant (rel-1.12.2-Forge)";
-
-            Wrapper.GetMC().fontRenderer.drawStringWithShadow(l_Text, GetX(), GetY(), 0xFFFFFF);
-
-            SetWidth(Wrapper.GetMC().fontRenderer.getStringWidth(l_Text));
-            SetHeight(Wrapper.GetMC().fontRenderer.FONT_HEIGHT);
+            l_welcome = ChatFormatting.AQUA + "Good Morning, " + ChatFormatting.WHITE + mc.getSession().getUsername();
+        }
+        else if (timeOfDay >= 12 && timeOfDay < 17)
+        {
+            l_welcome = ChatFormatting.AQUA + "Good Afternoon, " + ChatFormatting.WHITE + mc.getSession().getUsername();
+        }
+        else if (timeOfDay >= 17 && timeOfDay < 22)
+        {
+            l_welcome = ChatFormatting.AQUA + "Good Evening, " + ChatFormatting.WHITE + mc.getSession().getUsername();
+        }
+        else if (timeOfDay >= 22 || timeOfDay < 6)
+        {
+            l_welcome = ChatFormatting.AQUA + "Good Night, " + ChatFormatting.WHITE + mc.getSession().getUsername();
         }
         else
         {
-            RenderUtil.drawStringWithShadow(WelcomeString, GetX(), GetY(), 0x2ACCED);
-
-            SetWidth(RenderUtil.getStringWidth(WelcomeString));
-            SetHeight(RenderUtil.getStringHeight(WelcomeString));
+            l_welcome = ChatFormatting.AQUA + "Hello, " + ChatFormatting.WHITE + mc.getSession().getUsername() + ".. psst! something went wrong!";
         }
+
+        SetWidth(RenderUtil.getStringWidth(l_welcome));
+        SetHeight(RenderUtil.getStringHeight(l_welcome) + 1);
+
+        RenderUtil.drawStringWithShadow(l_welcome, GetX(), GetY(), 0x2ACCED);
     }
 }
